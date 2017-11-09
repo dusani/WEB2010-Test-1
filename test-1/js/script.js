935127cbc75f6567e94e13f2dc6ea64606374b3d
Daniel Usani <daniel.e.usani@gmail.com>
2017-11-08 19:02:41 -0500

    WEB2010 Test 1

---
+++ test-1/js/script.js
@@ -0,0 +1,63 @@
+// function randomColor() {
+//   var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' +
+//     (Math.floor(Math.random() * 256)) + ',' +
+//     (Math.floor(Math.random() * 256)) + ')';
+//
+//   return color;
+// }
+
+const boxes = document.querySelectorAll(".column");
+const colors = [
+  "rgb(61, 201, 64)",
+  "rgb(195, 66, 11)",
+  "rgb(162, 9, 84)",
+  "rgb(180, 130, 24)",
+  "rgb(90, 21, 120)",
+  "rgb(255, 0, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 127, 255)",
+  "rgb(255, 255, 127)"
+]
+
+boxes.forEach((element, i) => {
+  element.innerText = i + 1;
+  element.style.backgroundColor = colors[0];
+});
+
+
+class Squares {
+  constructor(boxes, colors) {
+    this.addClickHandler(boxes, colors)
+  }
+
+  changeNumber(i, boxes) {
+    console.log("Running changeNumber function");
+    for (var a = i; a >= 0; a--) {
+      const b = parseInt(boxes[a].innerText) + 1;
+      boxes[a].innerText = (b > 9) ? 1 : b;
+    }
+  }
+
+  changeColor(i, boxes, colors) {
+    console.log("Running changeColor function");
+    for (var a = i; a >= 0; a--) {
+      let style = window.getComputedStyle(boxes[a]);
+      let index = colors.indexOf(style.backgroundColor);
+      boxes[a].style.backgroundColor = (index > 7) ? colors[0] : colors[index + 1];
+      console.log(boxes[a].style.backgroundColor)
+      console.log(style.backgroundColor, index);
+    }
+  }
+
+  addClickHandler(boxes, colors) {
+    boxes.forEach((box, i) => {
+      box.addEventListener("click", evt => {
+        this.changeNumber(i, boxes);
+        this.changeColor(i, boxes, colors);
+      })
+    })
+  }
+}
+
+
+let squares = new Squares(boxes, colors);
